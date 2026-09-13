5b754f4b8b0f19d5b52f8a179d36ca9cb2041c25
YulongLee <1797358496+YulongLee@users.noreply.github.com>
2026-09-14 01:39:02 +0800

    fix: type app access token state

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,6 +3,7 @@
 interface IAppOption {
   globalData: {
     userInfo?: WechatMiniprogram.UserInfo,
+    accessToken?: string,
   }
   userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
